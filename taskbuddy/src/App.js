b1bed0851a404f851a4abd357ff6d94c1aae4e46
ramyanaik1904 <ramyalnaik04@gmail.com>
2024-12-26 11:53:43 +0530

    Added Taskform

--- taskbuddy/src/App.js
+++ taskbuddy/src/App.js
@@ -1,8 +1,59 @@
 
+import { useState, useEffect } from "react";
+import TaskForm from "./Components/Taskform";
+import './Style.css';
+import TaskList from "./Components/Tasklist";
+import ProgressTracker from "./components/ProgressTracker";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = (task)=>{
+    setTasks([...tasks,task])
+  };
+
+  const updateTask = (index, updatedTask) => {
+    const newTasks = [...tasks];
+    newTasks[index] = updatedTask;
+    setTasks(newTasks);
+  };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  const clearTasks = () => {
+    setTasks([]);
+  };
   return (
-    <h1>Hello</h1>
+    <div className='App'>
+      <header class="header">
+  <div class="header-container">
+    <h1 class="title">
+      Task<span class="highlight">Buddy</span>
+    </h1>
+    <p class="tagline">Your friendly task manager</p>
+  </div>
+</header>
+      <TaskForm addTask = {addTask} />
+      <TaskList
+        tasks={tasks}
+        updateTask={updateTask}
+        deleteTask={deleteTask}
+      />
+      <ProgressTracker tasks={tasks} />
+
+      {tasks.length>0 && (
+        <button className="clear-btn" onClick={clearTasks}>
+          Clear All Tasks
+        </button>
+       )}
+
+    </div>
   );
 }
 
